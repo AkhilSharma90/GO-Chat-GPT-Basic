67ea5197e9dca0274a802ea981b92986e4f5d854
AkhilSharma90 <akhilsails@gmail.com>
2023-01-16 17:19:30 +0530

    working, tested code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/akhil/go-chat-test
+
+go 1.18
+
+require (
+	github.com/PullRequestInc/go-gpt3 v1.1.11
+	github.com/joho/godotenv v1.4.0
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatalln("Missing API KEY")
+	}
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+
+	resp, err := client.Completion(ctx, gpt3.CompletionRequest{
+		Prompt:    []string{"The first thing you should know about javascript is"},
+		MaxTokens: gpt3.IntPtr(30),
+		Stop:      []string{"."},
+		Echo:      true,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(resp.Choices[0].Text)
+}
